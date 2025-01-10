2c3d5c37c8e810795b8dae0386167c7f3ee61a55
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2025-01-10 10:10:42 +0100

    back: setup entities models

---
+++ back/src/main/java/com/openclassrooms/mddapi/model/Comment.java
@@ -0,0 +1,43 @@
+package com.openclassrooms.mddapi.model;
+
+import java.sql.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Comment {
+
+    public static final int DESC_MAX_SIZE = 1024;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long id;
+
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "post_id", referencedColumnName = "id")
+    public Post post;
+
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "user_id", referencedColumnName = "id")
+    public User author;
+
+    @Column(nullable = false, length = DESC_MAX_SIZE)
+    public String content;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    public Date created_at;
+
+}
---
+++ back/src/main/java/com/openclassrooms/mddapi/model/Topic.java
@@ -0,0 +1,29 @@
+package com.openclassrooms.mddapi.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Topic {
+
+    public static final int TITLE_MAX_SIZE = 255;
+
+    public static final int DESC_MAX_SIZE = 1024;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long id;
+
+    @Column(nullable = false, length = TITLE_MAX_SIZE)
+    public String title;
+
+    @Column(nullable = false, length = DESC_MAX_SIZE)
+    public String description;
+}
