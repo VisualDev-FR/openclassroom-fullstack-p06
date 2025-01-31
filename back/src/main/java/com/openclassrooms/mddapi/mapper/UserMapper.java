ab9386a9f0fc45c6182f19acacc838aeb70c8614
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2025-01-31 09:09:05 +0100

    back: add mappers unmappedTargetPolicy

--- back/src/main/java/com/openclassrooms/mddapi/mapper/UserMapper.java
+++ back/src/main/java/com/openclassrooms/mddapi/mapper/UserMapper.java
@@ -1,13 +1,14 @@
 package com.openclassrooms.mddapi.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 import org.springframework.stereotype.Component;
 
 import com.openclassrooms.mddapi.dto.UserDto;
 import com.openclassrooms.mddapi.model.User;
 
 @Component
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface UserMapper extends EntityMapper<UserDto, User> {
 
 }
